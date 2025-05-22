5bb2d7cda94fd05064b14da6fad6204bf78a67ba
Lavida121 <lavidaloca2312@gmail.com>
2025-05-22 23:27:10 +0200

    Add files via upload

---
+++ api/tankstellen.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  const { lat, lng, rad = 5, type = 'e5' } = req.query;
+  const API_KEY = '7c70651b-0129-99e3-e7f3-b28255ddd859';
+  const url = `https://creativecommons.tankerkoenig.de/json/list.php?lat=${lat}&lng=${lng}&rad=${rad}&sort=price&type=${type}&apikey=${API_KEY}`;
+
+  try {
+    const response = await fetch(url, { timeout: 7000 });
+    const json = await response.json();
+    res.status(200).json(json);
+  } catch (error) {
+    console.error("API Fehler:", error.message);
+    res.status(500).json({ ok: false, message: 'Fehler beim Abrufen der Tankstellen.' });
+  }
+}
